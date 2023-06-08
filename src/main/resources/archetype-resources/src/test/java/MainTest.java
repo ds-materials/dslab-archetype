f2fb721bdcae93e7332b3ca3ba1e335fa4f526d0
Nathan Eloe <powerofazure@gmail.com>
2023-06-08 10:03:15 -0500

    Update for GitHub Classroom

---
+++ src/main/resources/archetype-resources/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+package ${package};
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+
+  @Test
+  void add_two_numbers() {
+    assertEquals(1+1, 2);
+  }
+}
